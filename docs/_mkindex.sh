6850721067f8441aa77b9563d3ece1693f9311d8
HexagonWin <hexagonwin@protonmail.com>
2021-07-12 20:17:00 -0400

    Update
    -Edit MKINDEX and add log function
    -Edit WhyMatrix.odp to edit wrong information

--- docs/_mkindex.sh
+++ docs/_mkindex.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 export CURDIR=$(basename "$PWD")
+echo "Starting mkindex in $(date)" >> _mkindex.log
+echo "Starting mkindex in $(date)"
+
+echo "Generating list of directory $CURDIR" >> _mkindex.log
 echo "Generating list of directory $CURDIR"
+
+ls >> _mkindex.log
 ls
+
+echo "Generating ..." >> _mkindex.log
 echo "Generating ..."
-ls | egrep '(INFORMATION-README|odp|txt|bin|docs|jpg|png|apk|zip|rar)' |  #filter the content you want in the index
+
+ls | egrep '(INFORMATION-README|odp|txt|bin|docs|jpg|png|apk|zip|rar)' |  #File include
 perl -e 'print "<html><body bgcolor=#dcf9fa><p><center>ahiSKY</center><p><center><h3>androians-hi SKY Developing</h3></center><p><center><hr noshade width=50%><font size=+2>Index of directory docs</font><ul><li><a href=..>.. (Parent)</a>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</li></ul><hr noshade width=50%></center><p><center></center><p><center><address>C. SysOP - &copy 2009 BKN Network Systems<br></adress>"' > index.html
-echo "Powered by mkindex. Last updated on $(date)</center></body></html>" >> index.html
-echo "Generated : Time $(date) previous"
+echo "Powered by mkindex. Last updated on $(date)</center></body></html>" >> index.html 
+echo "Generated automatically: Time $(date) previous"  >> _mkindex.log
+echo "Generated automatically: Time $(date) previous"
+
+echo "Here is the content of your file" >> _mkindex.log
 echo "Here is the content of your file"
+echo "================================" >> _mkindex.log
 echo "================================"
+cat index.html >> _mkindex.log
 cat index.html
+echo "================================" >> _mkindex.log
 echo "================================"
+echo "Script finished" >> _mkindex.log
 echo "Script finished"
+echo "Log is saved to _mkindex.log" >> _mkindex.log
+echo "Log is saved to _mkindex.log"
